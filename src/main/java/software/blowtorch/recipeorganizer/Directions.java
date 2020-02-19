14ffc1330bea3db77ddc9df94a77654ac256a1e8
vinny <mv.baker@gmail.com>
2020-02-18 23:33:48 -0500

    Initial commit

---
+++ src/main/java/software/blowtorch/recipeorganizer/Directions.java
@@ -0,0 +1,71 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package software.blowtorch.recipeorganizer;
+
+/**
+ *
+ * @author vinny
+ */
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import static software.blowtorch.recipeorganizer.DBHelper.*;
+
+
+public class Directions {
+    private int directionID;
+    private String direction;
+
+    protected Directions(String direction) {
+        this.direction = direction;
+        this.directionID = DBHelper.doesXExist(DIRECTIONS_TABLE, DIRECTION_ID, DIRECTION_NAME, direction);
+        if (this.directionID == 0) {
+            this.directionID = DBHelper.writeOneValueToDB(DIRECTIONS_TABLE, DIRECTION_NAME, direction);
+        }
+    }
+
+    protected int getDirectionID() { return this.directionID; }
+    protected String getDirection() { return this.direction; }
+
+    protected void setDirection(String d) { this.direction = d; }
+
+    protected static ArrayList<Directions> getDirectionsByRecipeID(int id) {
+        Connection conn = null;
+        PreparedStatement stmt = null;
+        ResultSet rs = null;
+        ArrayList<Directions> dir = new ArrayList<>();
+        try {
+            conn = DBHelper.connectDB();
+            String sql = "SELECT " + DIRECTION_NAME + " FROM " + DIRECTIONS_TABLE + "," + DIRECTIONS_RECIPE_TABLE +
+                    " WHERE " + DIRECTION_ID + "=" + DIRECTIONS_RECIPE_ID + " AND " + RECIPE_FK_DIRECTION + "=?";
+            stmt = conn.prepareStatement(sql);
+            stmt.setInt(1, id);
+            rs = stmt.executeQuery();
+            if (rs.next() != false) {
+                do {
+                    Directions d = new Directions(rs.getString(DIRECTION_NAME));
+                    dir.add(d);
+                } while (rs.next());
+                return dir;
+            }
+            return dir;
+        } catch (SQLException ex) {
+            System.err.println("Exception :" + ex.getMessage() + " in getDirectionsByRecipeID");
+            return dir;
+        } finally {
+            try {
+                rs.close();
+                stmt.close();
+                conn.close();
+            } catch (SQLException ex) {
+                System.err.println("Exception :" + ex.getMessage() + " in getDirectionsByRecipeID");
+            }
+        }
+    }
+}
